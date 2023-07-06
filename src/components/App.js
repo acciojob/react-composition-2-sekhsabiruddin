b1eff029ea2b988fe1ae74608b90823cc80f85ee
sabir <sksabiruddin81@gmail.com>
2023-07-06 12:26:03 +0530

    submitting code-Thu Jul 06 2023 12:25:56 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  let [show, setShow] = useState(false);
   return (
     <div>
-        {/* Do not remove the main div */}
+      <button onClick={() => setShow(true)}>Show Modal</button>
+      {show && <Modal showprop={setShow} />}
     </div>
-  )
+  );
+};
+
+function Modal({ showprop }) {
+  return (
+    <div className="modal">
+      <div className="modal-overlay" onClick={() => showprop(false)}>
+        <button className="modal-close" onClick={() => showprop(false)}>
+          Close
+        </button>
+        <p>This is the content of the modal.</p>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
